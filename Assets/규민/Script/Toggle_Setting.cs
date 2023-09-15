b01618ed074d2df9e58ad0973a770cb8f9c8a328
dhf0527 <rbals0527@naver.com>
2023-09-15 18:01:26 +0900

    세팅창 구현중

---
+++ Assets/규민/Script/Toggle_Setting.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle_Setting : MonoBehaviour
+{
+    public Enum_UI.Tg_Setting tg_setting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
