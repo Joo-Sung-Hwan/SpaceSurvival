c6f3a38fef06879c89d1e8a66187c069c9570de2
dhf0527 <rbals0527@naver.com>
2023-09-19 18:34:35 +0900

    에셋 추가 (tile)

---
+++ Assets/규민/Script/PlayerInventoryController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Inventory_ItemData
+{
+    public string itemName;
+    public string rare;
+    public Item_ability ia;
+}
+
+public struct Item_ability
+{
+    public string abilityName_1;
+    public float abilityValue;
+}
+
+public class PlayerInventoryController : MonoBehaviour
+{
+
+}
+
+public class PlayerInventoryData
+{
+
+}
