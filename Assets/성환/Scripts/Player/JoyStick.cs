ede9d4d743fe6c1cbcb9027fe180f3eb2d5f7e80
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-09-26 19:51:30 +0900

    플레이어 플립 수정

--- Assets/성환/Scripts/Player/JoyStick.cs
+++ Assets/성환/Scripts/Player/JoyStick.cs
@@ -60,11 +60,11 @@ public class JoyStick : MonoBehaviour, IBeginDragHandler,IEndDragHandler,IDragHa
         {
             if(input_vector.x > 0)
             {
-                p.GetComponent<Transform>().localScale = new Vector3(-1f, 1f, 1f);
+                p.GetComponent<SpriteRenderer>().flipX = true;
             }
             else
             {
-                p.GetComponent<Transform>().localScale = new Vector3(1f, 1f, 1f);
+                p.GetComponent<SpriteRenderer>().flipX = false;
             }
             p.GetComponent<CharacterController>().Move(new Vector2(input_vector.x, input_vector.y) * Time.deltaTime * Speed);
             p.ps = PlayerState.Walk;
