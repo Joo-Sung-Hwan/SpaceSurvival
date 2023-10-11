043e9286e22cb2c0644de86fad7a0795052f4d2d
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-10-11 19:57:59 +0900

    레이저 구현중
    살려주세요.......

--- Assets/성환/Scripts/Bullet/Laser.cs
+++ Assets/성환/Scripts/Bullet/Laser.cs
@@ -8,15 +8,19 @@ public class Laser : MonoBehaviour
     int reflectcount;
     Vector2 nextDir;
     float roZ;
+    RaycastHit2D ray;
+    float maxDistance;
+     int layermask;
     public void Start()
     {
+        layermask = 1 << 9;
         SetData();
         ReflectMaxCount = 5;
-        
     }
 
     void Update()
     {
+        Debug.DrawRay(transform.position, nextDir * 100f, Color.blue);
         transform.position += (Vector3)nextDir * Time.deltaTime * 3f;
         DestroyLaser();
     }
@@ -35,7 +39,7 @@ public class Laser : MonoBehaviour
         nextDir = GameManager.instance.GetRandomPosition(GameManager.instance.spawnManager.spawnPoint[rand].transform, GameManager.instance.spawnManager.spawnPoint[rand]).normalized;
         roZ = GetAngle(GameManager.instance.playerSpawnManager.player.transform.position, nextDir);
         transform.rotation = Quaternion.Euler(0, 0, roZ);
-
+        ray = Physics2D.Raycast(transform.position, nextDir, maxDistance, layermask);
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -45,16 +49,19 @@ public class Laser : MonoBehaviour
             Vector2 income = nextDir;
             Vector2 normal = collision.contacts[0].normal;
             nextDir = Vector2.Reflect(income, normal).normalized;
-            roZ = GetAngle(normal, nextDir);
+            roZ = 180 - roZ;
             transform.rotation = Quaternion.Euler(0, 0, roZ);
             reflectcount++;
         }        
     }
+
+    
     public float GetAngle(Vector2 b_dir, Vector2 a_dir)
     {
         Vector2 v = a_dir - b_dir;
         return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
     }
 
+   
     
 }
