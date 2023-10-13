a6f311eb000046317cafc743a463402d101f857d
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-10-13 17:55:01 +0900

    Update Laser.cs

--- Assets/성환/Scripts/Bullet/Laser.cs
+++ Assets/성환/Scripts/Bullet/Laser.cs
@@ -9,10 +9,12 @@ public class Laser : MonoBehaviour
     Vector2 nextDir;
     float roZ;
     RaycastHit2D ray;
+    RaycastHit ray2;
     public LayerMask layermask;
     LineRenderer lr;
     public void Start()
     {
+        
         lr = GetComponent<LineRenderer>();
         lr.startColor = Color.blue;
         lr.endColor = Color.blue;
@@ -25,7 +27,7 @@ public class Laser : MonoBehaviour
 
     void Update()
     {
-       // Debug.DrawRay(transform.position, nextDir, Color.blue);
+        Debug.DrawRay(transform.position, nextDir, Color.blue);
         transform.position += (Vector3)nextDir * Time.deltaTime * 3f;
         DestroyLaser();
     }
@@ -56,13 +58,50 @@ public class Laser : MonoBehaviour
         lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, -1f));
         for(int i = 1; i < lr.positionCount; i++)
         {
-            ray = Physics2D.Raycast(newposition, newDir, 100f, layermask);
+            OffCollider();
+            ray = Physics2D.Raycast(newposition, newDir, 300f, layermask);
+            Debug.DrawRay(newposition, newDir, Color.blue);
             lr.SetPosition(i, new Vector3(ray.point.x, ray.point.y, -1f));
             newposition = ray.point;
+            Debug.Log($"newDir = {newDir}, ray.nomral = {ray.normal}");
+
+
             newDir = Vector2.Reflect(newDir, ray.normal);
+            Debug.Log(newDir);
+
+            ray.collider.gameObject.SetActive(false);
+        }
+        OffCollider();
+    }
+
+    public void OffCollider()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            GameManager.instance.spawnManager.spawnPoint[i].gameObject.SetActive(true);
         }
     }
-    
+
+    public void ShootRay2()
+    {
+        Vector3 newposition = transform.position; ;
+        Vector3 newDir = new Vector3(nextDir.x, nextDir.y, 1f);
+        Debug.Log(newDir);
+
+        lr.positionCount = ReflectMaxCount;
+        lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, -1f));
+        for (int i = 1; i < lr.positionCount; i++)
+        {
+            Physics.Raycast(newposition, newDir, out ray2, 100f, layermask);
+            //Physics.Raycast(newposition, newDir,  100f, layermask);
+            
+            lr.SetPosition(i, new Vector3(ray2.point.x, ray2.point.y, -1f));
+            newposition = ray2.point;
+            newDir = Vector3.Reflect(newDir, ray2.normal);
+
+        }
+    }
+
 
 
     public float GetAngle(Vector2 b_dir, Vector2 a_dir)
