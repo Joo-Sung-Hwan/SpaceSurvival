bb206f10034ad90df981a00caf4bab92972e87c4
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-10-16 19:56:47 +0900

    레이저 완료!!!

--- Assets/성환/Scripts/Bullet/Laser.cs
+++ Assets/성환/Scripts/Bullet/Laser.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     [HideInInspector] public int ReflectMaxCount { get; set; }
+    public LaserChild lc;
     int reflectcount;
     Vector2 nextDir;
     float roZ;
@@ -14,7 +15,6 @@ public class Laser : MonoBehaviour
     LineRenderer lr;
     public void Start()
     {
-        
         lr = GetComponent<LineRenderer>();
         lr.startColor = Color.blue;
         lr.endColor = Color.blue;
@@ -22,87 +22,69 @@ public class Laser : MonoBehaviour
         lr.endWidth = 0.1f;
         ReflectMaxCount = 5;
         SetData();
-        
     }
 
     void Update()
     {
-        Debug.DrawRay(transform.position, nextDir, Color.blue);
-        transform.position += (Vector3)nextDir * Time.deltaTime * 3f;
         DestroyLaser();
     }
     public void DestroyLaser()
     {
-        if(reflectcount >= ReflectMaxCount)
+        if (reflectcount >= ReflectMaxCount)
         {
             gameObject.SetActive(false);
         }
     }
-    
+
     public void SetData()
     {
         int rand = Random.Range(0, 4);
         reflectcount = 0;
+
+        // 레이저 발사 첫 방향은 테두리 4방향 랜덤 좌표
         nextDir = GameManager.instance.GetRandomPosition(GameManager.instance.spawnManager.spawnPoint[rand].transform, GameManager.instance.spawnManager.spawnPoint[rand]).normalized;
-        roZ = GetAngle(GameManager.instance.playerSpawnManager.player.transform.position, nextDir);
-        transform.rotation = Quaternion.Euler(0, 0, roZ);
         ShootRay();
     }
-    
+
+    // LineRenderer, 레이저 발사
     public void ShootRay()
     {
-        Vector2 newposition = transform.position; ;
+        Vector2 newposition = transform.position;
         Vector2 newDir = nextDir;
-        
         lr.positionCount = ReflectMaxCount;
-        lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, -1f));
-        for(int i = 1; i < lr.positionCount; i++)
+        float rotZ;
+        for (int i = 0; i < lr.positionCount; i++)
         {
-            OffCollider();
+            if (i == 0)
+            {
+                lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, 0f));
+                continue;
+            }
             ray = Physics2D.Raycast(newposition, newDir, 300f, layermask);
-            Debug.DrawRay(newposition, newDir, Color.blue);
-            lr.SetPosition(i, new Vector3(ray.point.x, ray.point.y, -1f));
+            LaserChild c = Instantiate(lc, transform);
+            lr.SetPosition(i, new Vector3(ray.point.x, ray.point.y, 0f));
             newposition = ray.point;
-            //Debug.Log($"newDir = {newDir}, ray.nomral = {ray.normal}");
-
-
-            newDir = Vector2.Reflect(newDir, ray.normal);
-            //Debug.Log(newDir);
-
+            c.transform.position = lr.GetPosition(i - 1);
+            rotZ = GetAngle(lr.GetPosition(i - 1), lr.GetPosition(i));
+            c.transform.rotation = Quaternion.Euler(0, 0, rotZ);
+            c.GetComponent<SpriteRenderer>().size = new Vector2(Vector2.Distance(lr.GetPosition(i - 1), lr.GetPosition(i)) + 1f, 0.3f);
+            // Debug.Log($"i = {i}, 시작지점 : {c.transform.position}, 다음지점 : {lr.GetPosition(i)}, Rotation = {rotZ} ");
+            OnCollider();
             ray.collider.gameObject.SetActive(false);
+            newDir = Vector2.Reflect(newDir, ray.normal);
+
         }
-        OffCollider();
+        OnCollider();
     }
 
-    public void OffCollider()
+    // 콜라인더 SetActive
+    public void OnCollider()
     {
-        for(int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; i++)
         {
             GameManager.instance.spawnManager.spawnPoint[i].gameObject.SetActive(true);
         }
     }
-
-    public void ShootRay2()
-    {
-        Vector3 newposition = transform.position; ;
-        Vector3 newDir = new Vector3(nextDir.x, nextDir.y, 1f);
-        Debug.Log(newDir);
-
-        lr.positionCount = ReflectMaxCount;
-        lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, -1f));
-        for (int i = 1; i < lr.positionCount; i++)
-        {
-            Physics.Raycast(newposition, newDir, out ray2, 100f, layermask);
-            //Physics.Raycast(newposition, newDir,  100f, layermask);
-            
-            lr.SetPosition(i, new Vector3(ray2.point.x, ray2.point.y, -1f));
-            newposition = ray2.point;
-            newDir = Vector3.Reflect(newDir, ray2.normal);
-
-        }
-    }
-
-
 
     public float GetAngle(Vector2 b_dir, Vector2 a_dir)
     {
@@ -110,6 +92,6 @@ public class Laser : MonoBehaviour
         return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
     }
 
-   
-    
+
+
 }
