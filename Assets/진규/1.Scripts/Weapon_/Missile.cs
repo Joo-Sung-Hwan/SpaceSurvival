a8ea0c69ade0d6bc4e86717b6b9a667ccb6b3e98
jiho1120 <98639630+cxz1113@users.noreply.github.com>
2023-11-21 21:53:58 +0900

    유도미사일

--- Assets/진규/1.Scripts/Weapon_/Missile.cs
+++ Assets/진규/1.Scripts/Weapon_/Missile.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class Missile : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
@@ -14,5 +13,19 @@ public class Missile : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FindEnemy()
+    {
+        List<Enemy> enemies = FindObjectsOfType<Enemy>().ToList();
+
+        for(int i = 0; i < 10; i++)
+        {
+            float distance = Vector3.Distance(transform.position, enemies[i].transform.position);
+            foreach(var enemy in enemies)
+            {
+                float dis = Vector3.Distance(transform.position, enemy.transform.position);
+            }
+        }
     }
 }
