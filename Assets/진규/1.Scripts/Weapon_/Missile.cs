e2f6c81c7d0809eca782344fc6ba641417bf639a
cxz1113 <98639630+cxz1113@users.noreply.github.com>
2023-11-22 21:46:58 +0900

    유도미사일 작업중

--- Assets/진규/1.Scripts/Weapon_/Missile.cs
+++ Assets/진규/1.Scripts/Weapon_/Missile.cs
@@ -4,28 +4,58 @@ using UnityEngine;
 using System.Linq;
 public class Missile : MonoBehaviour
 {
+    public List<Enemy> findEnemy;
+    public Missile_Bullet mi;
+    float time = 0;
+    //public Enemy e;
     void Start()
     {
-        
+        //ObjEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (time > 2)
+            Init();
+        FindEnemy();
+        //Find();
+        //Find();
     }
 
     void FindEnemy()
     {
-        List<Enemy> enemies = FindObjectsOfType<Enemy>().ToList();
-
-        for(int i = 0; i < 10; i++)
+        Enemy[] e = FindObjectsOfType<Enemy>().OrderBy(x => Vector2.Distance(transform.position, x.transform.position)).ToArray();
+        findEnemy.Clear();
+        for (int i = 0; i < e.Length; i++)
         {
-            float distance = Vector3.Distance(transform.position, enemies[i].transform.position);
-            foreach(var enemy in enemies)
-            {
-                float dis = Vector3.Distance(transform.position, enemy.transform.position);
-            }
+            findEnemy.Add(e[i]);
+            if (findEnemy.Count >= 6)
+                break;
         }
     }
+
+    void Init()
+    {
+
+        for (int i = 0; i < 6; i++)
+        {
+            Missile_Bullet missile_Bullet = Instantiate(mi, transform);
+            missile_Bullet.transform.SetParent(transform.GetChild(0));
+            //return missile_Bullet;
+        }
+    }
+    /*void Find()
+    {
+        
+        for (int i = 0; i < findEnemy.Count; i++)
+        {
+            Vector2 vec = mi[i].transform.position - findEnemy[i].transform.position;
+            float angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+            mi[i].transform.rotation = rotation;
+            mi[i].transform.Translate(Vector3.up * 2 * Time.deltaTime);
+        }
+    }*/
 }
