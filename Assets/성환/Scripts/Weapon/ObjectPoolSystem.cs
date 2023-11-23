fbc59f978f551ec70ac9f959632353b0eae5c038
jiho1120 <dlwlghdhk@gmail.com>
2023-11-23 21:31:54 +0900

    풀링수정

--- Assets/성환/Scripts/Weapon/ObjectPoolSystem.cs
+++ Assets/성환/Scripts/Weapon/ObjectPoolSystem.cs
@@ -2,35 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPoolSystem<T> : MonoBehaviour where T : Object
+public class ObjectPoolSystem : MonoBehaviour
 {
-    private static ObjectPoolSystem<T> instance = null;
-
-    public static ObjectPoolSystem<T> Instance { get { return instance; } }
-
-    private Dictionary<string, Queue<T>> pools = new Dictionary<string, Queue<T>>();
+    private static ObjectPoolSystem instance = null;
+    public static ObjectPoolSystem Instance { get { return instance; } }
     private void Awake()
     {
         if (Instance == null) instance = this;
         else Destroy(gameObject);
     }
-    public T GetPool(T value , Transform trans)
-    {
-        T item = default(T);
 
-        if (!pools.ContainsKey(value.ToString()))
-        {
-            pools.Add(value.ToString(), new Queue<T>());
-            item = Instantiate(value, trans);
-        }
-        else
-        {
-            item = pools[value.ToString()].Dequeue();
-        }
-        return item;
-    }
-    public void ReturnPool(T value)
+    public static class ObjectPoolling<T> where T : Object
     {
-        pools[value.ToString()].Enqueue(value);
+        private static Dictionary<int, Queue<T>> pools = new Dictionary<int, Queue<T>>();
+       
+        public static T GetPool(T value,int num, Transform trans)
+        {
+            T item = default(T);
+
+            if (!pools.ContainsKey(num))
+            {
+                pools.Add(num, new Queue<T>());
+                item = Instantiate(value, trans);
+            }
+            else if (pools.ContainsKey(num) && pools[num].Count ==0)
+            {
+                item = Instantiate(value, trans);
+            }
+            else
+            {
+                item = pools[num].Dequeue();
+            }
+            return item;
+        }
+        public static void ReturnPool(T value,int num)
+        {
+            pools[num].Enqueue(value);
+        }
     }
 }
