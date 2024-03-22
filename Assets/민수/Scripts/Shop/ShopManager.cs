60825b7e98c0ecd63699684fb40d4038cc854fe7
dhf0527 <rbals0527@naver.com>
2024-03-22 16:37:24 +0900

    Revert "Revert "애드몹 추가""
    This reverts commit df685edc61bd794fdc254000c925d57918c614a2.

--- Assets/민수/Scripts/Shop/ShopManager.cs
+++ Assets/민수/Scripts/Shop/ShopManager.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ShopManager : MonoBehaviour
+using System;
+using UnityEngine.UI;
+using TMPro;
+public abstract class ShopManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected List<ItemScriptableData> scriptableDatas = new List<ItemScriptableData>();
+    [SerializeField] protected Button buyButton;
+    [SerializeField] protected GameObject maskObj;
+    [SerializeField] protected GameObject buyCheckObj;
+
+    [SerializeField] Image product_Image;
+    [SerializeField] TMP_Text information_Text;
+    [SerializeField] Image check_Image;
+    [SerializeField] TMP_Text check_Text;
+
+    private void Awake()
     {
-        
+        buyButton.onClick.AddListener(() => OnClickBuyButtonDown());
+    }
+    protected virtual void OnClickBuyButtonDown()
+    {
+        maskObj.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+
+
+
+
+
+
+    public ItemData RandomItem()
     {
-        
+        int rand = UnityEngine.Random.Range(0, scriptableDatas.Count);
+        ItemScriptableData isd = scriptableDatas[rand];
+        ItemStaticData newIsData = new ItemStaticData(isd.ItemName, isd.Place, isd.WeaponKind, isd.SpriteName, isd.Description);
+
+        List<Item_Ability> newItemAbs = new List<Item_Ability>();
+
+        //랜덤으로 어빌리티의 종류/수치를 결정함
+        //Random.Range(최소 개수, 최대 개수+1)
+
+        int valCount = Enum.GetValues(typeof(Enum_GM.abilityName)).Length;
+        int randNum = UnityEngine.Random.Range(0, valCount);
+
+
+        for (int i = 0; i < UnityEngine.Random.Range(2, 6); i++)
+        {
+            Item_Ability item_Ab = new Item_Ability();
+
+            item_Ab.abilityName = (Enum_GM.abilityName)randNum;
+            item_Ab.abilityValue = 1;
+            item_Ab.abilityrarity = Enum_GM.Rarity.normal;
+
+
+            newItemAbs.Add(item_Ab);
+            //인수에 들어있는 람다식에 따라 리스트를 정렬
+            //-> abilityValue값이 크면 앞으로 정렬
+        }
+
+        return new ItemData(newIsData, Enum_GM.Rarity.normal, newItemAbs, 0);
     }
 }
